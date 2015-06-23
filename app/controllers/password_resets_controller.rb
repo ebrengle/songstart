22854b5d809fa2c16301fc01bc0011c0d6597830
Eric Brengle <eric.brengle@gmail.com>
2015-06-23 06:25:33 -0500

    Implemented password reset and user verification

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,47 +1,62 @@
 class PasswordResetsController < ApplicationController
-  skip_before_filter :require_login
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
-  # request password reset.
-  # you get here when the user entered his email in the reset password form and submitted it.
+  def new
+  end
+
   def create
-    @user = User.find_by_email(params[:email])
-
-    # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
-    @user.deliver_reset_password_instructions! if @user
-
-    # Tell the user instructions have been sent whether or not email was found.
-    # This is to not leak information to attackers about which emails exist in the system.
-    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
-  end
-
-  # This is the reset password form.
-  def edit
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
-
-    if @user.blank?
-      not_authenticated
-      return
-    end
-  end
-
-  # This action fires when the user has sent the reset password form.
-  def update
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
-
-    if @user.blank?
-      not_authenticated
-      return
-    end
-
-    # the next line makes the password confirmation validation work
-    @user.password_confirmation = params[:user][:password_confirmation]
-    # the next line clears the temporary token and updates the password
-    if @user.change_password!(params[:user][:password])
-      redirect_to(root_path, :notice => 'Password was successfully updated.')
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
     else
-      render :action => "edit"
+      flash.now[:danger] = "Email address not found"
+      render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      flash.now[:danger] = "Password can't be empty"
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+
 end
